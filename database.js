f3af31b7ad9fa65427df0d2640ca4958fa049e14
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2025-02-27 20:58:15 -0300

    att sticky

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 // ip/link do servidor, autenticação, nome do banco
 // ao final da url, definir o nome do banco de dados
 // exemplo: /dblanches
-const url = 'mongodb+srv://admin:123Unicamp@cluster2.b3nnl.mongodb.net/dblanches'
+const url = ''
 
 // validação (evitar a abertura de várias conexões)
 let conectado = false
